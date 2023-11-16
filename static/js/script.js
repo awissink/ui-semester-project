6f3291e0c345d0682d54b9cf9a4651afdcd3c633
Grace Perrin <ghp2121@barnard.edu>
2023-11-16 02:04:42 -0500

    nearly finished all form signup parts and created flow and skeleton code for the rest

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,41 @@
+//REDIRECTION FUNCTIONS
 function redirectToHomepage() {
     window.location.href = '../../templates/index.html';
+}
+function redirectToMenteeMessages() {
+    window.location.href = '../../templates/menteeMessages.html';
+}
+function redirectToMentorMessages() {
+    window.location.href = '../../templates/mentorMessages.html';
+}
+function redirectToMessages(role) {
+    console.log("redirecting to messages page for role:", role)
+    if (role === "mentee") {
+        redirectToMenteeMessages();
+    } else if (role === "mentor") {
+        redirectToMentorMessages();
+    }
+}
+
+//VALIDATE PASSWORDS
+function validatePassword(role, event) {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirm-password").value;
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        event.preventDefault(); // Prevent form submission
+    } else {
+        redirectToMessages(role);
+    }
+ 
+}
+
+//PROVIDE OPTION FOR TEXT BOX IF OTHER FIELD SELECTED
+function toggleOtherOption(formID) {
+    var select = document.getElementById(formID);
+    var otherInput = document.getElementById("other-" + formID);
+
+    otherInput.style.display = select.value === "other" ? "block" : "none";
+
 }
